893369f850cb166721097663ddbdcd365037e130
Miguel Quiroz <MQuiroz@acsicorp.com>
2022-02-21 14:31:40 -0600

    Get and Set antes de borrar

--- src/main/java/com/totaplay/searchONT/dto/RequestSearchOntDto.java
+++ src/main/java/com/totaplay/searchONT/dto/RequestSearchOntDto.java
@@ -13,6 +13,14 @@ import lombok.Data;
  */
 @Data
 public class RequestSearchOntDto {
-      private String serialNumber; 
+      public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	private String serialNumber; 
 }
 
--- src/main/java/com/totaplay/searchONT/dto/ResponseSearchOntDto.java
+++ src/main/java/com/totaplay/searchONT/dto/ResponseSearchOntDto.java
@@ -20,5 +20,29 @@ public class ResponseSearchOntDto {
     private Boolean error=false;
     private String message="";
     private String transactionLog =null;
+	public List<ModelGetONT> getList() {
+		return List;
+	}
+	public void setList(List<ModelGetONT> list) {
+		List = list;
+	}
+	public Boolean getError() {
+		return error;
+	}
+	public void setError(Boolean error) {
+		this.error = error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getTransactionLog() {
+		return transactionLog;
+	}
+	public void setTransactionLog(String transactionLog) {
+		this.transactionLog = transactionLog;
+	}
     
 }
